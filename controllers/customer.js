4e4656d6ee733b1e8bdd5fd8f4cd6b9fd71f13dd
Abdullah Fayad <abdullah0fayad@gmail.com>
2022-05-25 20:18:48 +0300

    add functions

--- controllers/customer.js
+++ controllers/customer.js
@@ -10,23 +10,59 @@ class Customer {
       customerSchema
         .create(body)
         .then(() => {
-          res.json({ message: "success" });
+          res.json({ message: "Add Successful" });
         })
         .catch((err) => {
-          res.json({ message: "fail" });
+          res.json({ message: "Add Failed" });
         });
     } else {
       res.send({ error: "The phone number is not valid" });
     }
   };
 
-  delete = (req, res) => {};
+  delete = (req, res) => {
+    let id = req?.params?.id;
+    customerSchema
+      .findByIdAndDelete(id)
+      .then(() => {
+        res.json({ message: "Delete Successful" });
+      })
+      .catch((err) => {
+        res.json({ message: "Delete Failed" });
+      });
+  };
 
-  edit = (req, res) => {};
+  update = async (req, res) => {
+    let body = req?.body;
+    let id = body?.id;
+    let number = body?.phoneNumber;
+    let validation = await validate(number);
+    if (validation?.valid) {
+      customerSchema
+        .findByIdAndUpdate(id, body, { new: "true" })
+        .then(() => {
+          res.json({ message: "Update Successful" });
+        })
+        .catch((err) => {
+          res.json({ message: "Update Failed" });
+        });
+    } else {
+      res.send({
+        error: "Update failed due to the invalidation of the phone number",
+      });
+    }
+  };
 
-  get = (req, res) => {};
-
-  getAll = (req, res) => {};
+  getAll = (req, res) => {
+    customerSchema
+      .find()
+      .then((customers) => {
+        res.json({ message: "Found All Customers", customers });
+      })
+      .catch((err) => {
+        res.json({ message: "Any Customer Not Found" });
+      });
+  };
 }
 const customer = new Customer();
 module.exports = customer;
