c3b08d33f97a0f61ffa8dd5eec899bf2dd7158ab
Abdullah Fayad <abdullah0fayad@gmail.com>
2022-05-25 20:17:52 +0300

    add 'cors'

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser')
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var validateNumberRouter = require('./routes/validateNumber');
 var customerRouter = require('./routes/customer');
 
 var app = express();
-app.use(bodyParser.json()); 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +21,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/number', validateNumberRouter);
